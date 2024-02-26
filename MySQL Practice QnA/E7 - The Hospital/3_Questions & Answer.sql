2630d3deb9cf84a346943efb51758f062dad7463
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2024-02-26 13:56:49 +0530

    Update 3_Questions & Answer.sql

--- MySQL Practice QnA/E7 - The Hospital/3_Questions & Answer.sql
+++ MySQL Practice QnA/E7 - The Hospital/3_Questions & Answer.sql
@@ -1,138 +1,150 @@
---3.1 Select all warehouses.
-select * from warehouses;
+-- 7.1 Obtain the names of all physicians that have performed a medical procedure they have never been certified to perform.
+SELECT NAME 
+FROM
+	Physician 
+WHERE
+	EmployeeID IN ( SELECT Physician FROM Undergoes U WHERE NOT EXISTS ( SELECT * FROM Trained_In WHERE Treatment = Procedures AND Physician = U.Physician ) ); 
 
 
---3.2 Select all boxes with a value larger than $150.
-select * from boxes where Value>150;
+-- 7.2 Same as the previous query, but include the following information in the results: Physician name, name of procedure, date when the procedure was carried out, name of the patient the procedure was carried out on.
+SELECT
+	P.NAME AS Physician,
+	Pr.NAME AS `procedure`,
+	U.DateUndergoes,
+	Pt.NAME AS Patient 
+FROM
+	Physician P,
+	Undergoes U,
+	Patient Pt,
+	Procedures Pr 
+WHERE
+	U.Patient = Pt.SSN 
+	AND U.Procedures = Pr.CODE 
+	AND U.Physician = P.EmployeeID 
+	AND NOT EXISTS ( SELECT * FROM Trained_In T WHERE T.Treatment = U.Procedures AND T.Physician = U.Physician );
 
 
---3.3 Select all distinct contents in all the boxes.
-select distinct contents from boxes;
+-- 7.3 Obtain the names of all physicians that have performed a medical procedure that they are certified to perform, but such that the procedure was done at a date (Undergoes.Date) after the physician's certification expired (Trained_In.CertificationExpires).
+SELECT
+	P.NAME 
+FROM
+	Physician AS P,
+	Trained_In T,
+	Undergoes AS U 
+WHERE
+	T.Physician = U.Physician 
+	AND T.Treatment = U.Procedures
+	AND U.DateUndergoes > T.CertificationExpires 
+	AND P.EmployeeID = U.Physician
 
 
---3.4Select the average value of all the boxes.
-select avg(value) from boxes;
+-- 7.4 Same as the previous query, but include the following information in the results: Physician name, name of procedure, date when the procedure was carried out, name of the patient the procedure was carried out on, and date when the certification expired.
+SELECT
+	P.NAME AS Physician,
+	Pr.NAME AS `PROCEDURE`,
+	U.DateUndergoes,
+	Pt.NAME AS Patient,
+	T.CertificationExpires 
+FROM
+	Physician P,
+	Undergoes U,
+	Patient Pt,
+	Procedures Pr,
+	Trained_In T 
+WHERE
+	U.Patient = Pt.SSN 
+	AND U.Procedures = Pr.CODE 
+	AND U.Physician = P.EmployeeID 
+	AND Pr.CODE = T.Treatment 
+	AND P.EmployeeID = T.Physician 
+	AND U.DateUndergoes > T.CertificationExpires;
 
 
---3.5 Select the warehouse code and the average value of the boxes in each warehouse.
-select warehouse, avg(value) from boxes group by warehouse;
+-- 7.5 Obtain the information for appointments where a patient met with a physician other than his/her primary care physician. Show the following information: Patient name, physician name, nurse name (if any), start and end time of appointment, examination room, and the name of the patient's primary care physician.
+SELECT
+	Pt.NAME AS Patient,
+	Ph.NAME AS Physician,
+	N.NAME AS Nurse,
+	A.START,
+	A.END,
+	A.ExaminationRoom,
+	PhPCP.NAME AS PCP 
+FROM
+	Patient Pt,
+	Physician Ph,
+	Physician PhPCP,
+	Appointment A
+	LEFT JOIN Nurse N ON A.PrepNurse = N.EmployeeID 
+WHERE
+	A.Patient = Pt.SSN 
+	AND A.Physician = Ph.EmployeeID 
+	AND Pt.PCP = PhPCP.EmployeeID 
+	AND A.Physician <> Pt.PCP;
 
 
---3.6 Same as previous exercise, but select only those warehouses where the average value of the boxes is greater than 150.
-select warehouse, avg(value) 
-from boxes 
-group by warehouse
-having avg(value)> 150;
+-- 7.6 The Patient field in Undergoes is redundant, since we can obtain it from the Stay table. There are no constraints in force to prevent inconsistencies between these two tables. More specifically, the Undergoes table may include a row where the patient ID does not match the one we would obtain from the Stay table through the Undergoes.Stay foreign key. Select all rows from Undergoes that exhibit this inconsistency.
+SELECT
+	* 
+FROM
+	Undergoes U 
+WHERE
+	Patient <> ( SELECT Patient FROM Stay S WHERE U.Stay = S.StayID );
 
 
---3.7 Select the code of each box, along with the name of the city the box is located in.
-select boxes.code, warehouses.location
-from boxes join warehouses
-on boxes.Warehouse = Warehouses.Code;
+-- 7.7 Obtain the names of all the nurses who have ever been on call for room 123.
+SELECT
+	N.NAME 
+FROM
+	Nurse N 
+WHERE
+	EmployeeID IN (
+	SELECT
+		OC.Nurse 
+	FROM
+		On_Call OC,
+		Room R 
+	WHERE
+		OC.BlockFloor = R.BlockFloor 
+		AND OC.BlockCode = R.BlockCode 
+	AND R.RoomNumber = 123 
+	);
 
 
---3.8 Select the warehouse codes, along with the number of boxes in each warehouse. 
--- Optionally, take into account that some warehouses are empty (i.e., the box count should show up as zero, instead of omitting the warehouse from the result).
-select Warehouse, count(*) 
-from boxes 
-group by warehouse;
+-- 7.8 The hospital has several examination rooms where appointments take place. Obtain the number of appointments that have taken place in each examination room.
+SELECT ExaminationRoom, COUNT(*) AS Number FROM Appointment
+GROUP BY ExaminationRoom;
 
 
---3.9 Select the codes of all warehouses that are saturated (a warehouse is saturated if the number of boxes in it is larger than the warehouse's capacity).
-select Code
-from warehouses join (select warehouse temp_a, count(*) temp_b from boxes group by warehouse) temp
-on (warehouses.code = temp.temp_a)
-where warehouses.Capacity<temp.temp_b;
-
-	--	OR
-
-SELECT Code
-   FROM Warehouses
-   WHERE Capacity <
-   (
-     SELECT COUNT(*)
-       FROM Boxes
-       WHERE Warehouse = Warehouses.Code
-   );
-
-
-
---3.10 Select the codes of all the boxes located in Chicago.
-
-select Boxes.code 
-from boxes join Warehouses
-on boxes.warehouse = warehouses.code
-where warehouses.location = 'Chicago';
-
-/* Without subqueries */
- SELECT Boxes.Code
-   FROM Warehouses LEFT JOIN Boxes
-   ON Warehouses.Code = Boxes.Warehouse
-   WHERE Location = 'Chicago';
-
- /* With a subquery */
- SELECT Code
-   FROM Boxes
-   WHERE Warehouse IN
-   (
-     SELECT Code
-       FROM Warehouses
-       WHERE Location = 'Chicago'
-   );
-
-
---3.11 Create a new warehouse in New York with a capacity for 3 boxes.
-INSERT INTO Warehouses VALUES (6, 'New York', 3);
-
-
---3.12 Create a new box, with code "H5RT", containing "Papers" with a value of $200, and located in warehouse 2.
-INSERT INTO Boxes VALUES('H5RT', 'Papers', 200, 2);
-
-
---3.13 Reduce the value of all boxes by 15%.
-update boxes
-set value = value * 0.85;
-
-
---3.14 Remove all boxes with a value lower than $100.
-delete from boxes 
-where value < 100;
-
--- 3.15 Remove all boxes from saturated warehouses.
-delete from boxes
-where warehouse in
-(
-SELECT Code
-   FROM Warehouses
-   WHERE Capacity <
-   (
-     SELECT COUNT(*)
-       FROM Boxes
-       WHERE Warehouse = Warehouses.Code
-   )
-);
-
--- 3.16 Add Index for column "Warehouse" in table "boxes"
--- !!!NOTE!!!: index should NOT be used on small tables in practice
-CREATE INDEX INDEX_WAREHOUSE ON Boxes (warehouse);
-
--- 3.17 Print all the existing indexes
--- !!!NOTE!!!: index should NOT be used on small tables in practice
-
--- MySQL
-SHOW INDEX FROM Boxes FROM mydb;
-SHOW INDEX FROM mydb.Boxes;
-
--- SQLite
-.indexes Boxes
--- OR
-SELECT * FROM SQLITE_MASTER WHERE type = "index";
-
--- Oracle
-select INDEX_NAME, TABLE_NAME, TABLE_OWNER 
-from SYS.ALL_INDEXES 
-order by TABLE_OWNER, TABLE_NAME, INDEX_NAME
-
--- 3.18 Remove (drop) the index you added just
--- !!!NOTE!!!: index should NOT be used on small tables in practice
-DROP INDEX INDEX_WAREHOUSE;
+-- 7.9 Obtain the names of all patients (also include, for each patient, the name of the patient's primary care physician), such that \emph{all} the following are true:
+-- 
+-- The patient has been prescribed some medication by his/her primary care physician.
+-- The patient has undergone a procedure with a cost larger that $5,000
+-- The patient has had at least two appointment where the nurse who prepped the appointment was a registered nurse.
+-- The patient's primary care physician is not the head of any department.
+SELECT
+	Pt.NAME,
+	PhPCP.NAME 
+FROM
+	Patient Pt,
+	Physician PhPCP 
+WHERE
+	Pt.PCP = PhPCP.EmployeeID 
+	AND EXISTS ( 
+		SELECT * FROM Prescribes Pr WHERE 
+			Pr.Patient = Pt.SSN 
+			AND Pr.Physician = Pt.PCP 
+	) 
+	AND EXISTS (
+		SELECT * FROM Undergoes U, Procedures Pr WHERE 
+			U.Procedures = Pr.CODE 
+			AND U.Patient = Pt.SSN 
+			AND Pr.Cost > 5000 
+	) 
+	AND 2 <= (
+		SELECT COUNT( A.AppointmentID ) FROM Appointment A, Nurse N WHERE
+			A.PrepNurse = N.EmployeeID 
+			AND N.Registered = 1 
+	) 
+	AND NOT Pt.PCP IN ( 
+		SELECT Head FROM Department 
+	);
