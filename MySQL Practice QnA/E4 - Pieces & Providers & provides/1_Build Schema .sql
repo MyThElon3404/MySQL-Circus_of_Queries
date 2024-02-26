ba09be13915934e6c8530ab026ad851ee76ba1bd
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2024-02-26 12:02:55 +0530

    Update 1_Build Schema .sql

--- MySQL Practice QnA/E4 - Pieces & Providers & provides/1_Build Schema .sql
+++ MySQL Practice QnA/E4 - Pieces & Providers & provides/1_Build Schema .sql
@@ -1,39 +1,48 @@
--- Create tables -> Departments & Employees
+-- Create tables -> Pieces & Providers & Provides
 -- ===============================================================================
 
-drop table if exists Departments;
-CREATE TABLE Departments (
-  Code INTEGER PRIMARY KEY,
-  Name varchar(255) NOT NULL ,
-  Budget decimal NOT NULL 
-);
+drop table if exists Pieces;
+CREATE TABLE if not exists Pieces (
+ Code INTEGER PRIMARY KEY NOT NULL,
+ Name TEXT NOT NULL
+ );
 
-drop table if exists Employees;
-CREATE TABLE Employees (
-  SSN INTEGER PRIMARY KEY,
-  Name varchar(255) NOT NULL ,
-  LastName varchar(255) NOT NULL ,
-  Department INTEGER NOT NULL, 
-  foreign key (department) references Departments(Code) 
-);
+drop table if exists Providers;
+CREATE TABLE Providers (
+ Code VARCHAR(40) 
+ PRIMARY KEY NOT NULL,  
+ Name TEXT NOT NULL 
+ );
 
-INSERT INTO Departments(Code,Name,Budget) VALUES(14,'IT',65000);
-INSERT INTO Departments(Code,Name,Budget) VALUES(37,'Accounting',15000);
-INSERT INTO Departments(Code,Name,Budget) VALUES(59,'Human Resources',240000);
-INSERT INTO Departments(Code,Name,Budget) VALUES(77,'Research',55000);
+drop table if exists Provides;
+CREATE TABLE Provides (
+ Piece INTEGER, 
+ FOREIGN KEY (Piece) REFERENCES Pieces(Code),
+ Provider VARCHAR(40), 
+ FOREIGN KEY (Provider) REFERENCES Providers(Code),  
+ Price INTEGER NOT NULL,
+ PRIMARY KEY(Piece, Provider) 
+ );
 
-INSERT INTO Employees(SSN,Name,LastName,Department) VALUES('123234877','Michael','Rogers',14);
-INSERT INTO Employees(SSN,Name,LastName,Department) VALUES('152934485','Anand','Manikutty',14);
-INSERT INTO Employees(SSN,Name,LastName,Department) VALUES('222364883','Carol','Smith',37);
-INSERT INTO Employees(SSN,Name,LastName,Department) VALUES('326587417','Joe','Stevens',37);
-INSERT INTO Employees(SSN,Name,LastName,Department) VALUES('332154719','Mary-Anne','Foster',14);
-INSERT INTO Employees(SSN,Name,LastName,Department) VALUES('332569843','George','ODonnell',77);
-INSERT INTO Employees(SSN,Name,LastName,Department) VALUES('546523478','John','Doe',59);
-INSERT INTO Employees(SSN,Name,LastName,Department) VALUES('631231482','David','Smith',77);
-INSERT INTO Employees(SSN,Name,LastName,Department) VALUES('654873219','Zacary','Efron',59);
-INSERT INTO Employees(SSN,Name,LastName,Department) VALUES('745685214','Eric','Goldsmith',59);
-INSERT INTO Employees(SSN,Name,LastName,Department) VALUES('845657245','Elizabeth','Doe',14);
-INSERT INTO Employees(SSN,Name,LastName,Department) VALUES('845657246','Kumar','Swamy',14);
+INSERT INTO Pieces(Code, Name) VALUES(1,'Sprocket');
+INSERT INTO Pieces(Code, Name) VALUES(2,'Screw');
+INSERT INTO Pieces(Code, Name) VALUES(3,'Nut');
+INSERT INTO Pieces(Code, Name) VALUES(4,'Bolt');
+ 
+INSERT INTO Providers(Code, Name) VALUES('HAL','Clarke Enterprises');
+INSERT INTO Providers(Code, Name) VALUES('RBT','Susan Calvin Corp.');
+INSERT INTO Providers(Code, Name) VALUES('TNBC','Skellington Supplies');
+
+INSERT INTO Provides(Piece, Provider, Price) VALUES(1,'HAL',10);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(1,'RBT',15);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(2,'HAL',20);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(2,'RBT',15);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(2,'TNBC',14);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(3,'RBT',50);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(3,'TNBC',45);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(4,'HAL',5);
+INSERT INTO Provides(Piece, Provider, Price) VALUES(4,'RBT',7);
+
 
 
 
