5952d3741cd817dbb47d0c544f9e5c130ac6460b
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2024-02-26 13:57:45 +0530

    Update 3_Questions & Answer.sql

--- MySQL Practice QnA/E6 - Employee & Planet & Shipment/3_Questions & Answer.sql
+++ MySQL Practice QnA/E6 - Employee & Planet & Shipment/3_Questions & Answer.sql
@@ -1,4 +1,4 @@
--- 7.1 Who received a 1.5kg package?
+-- 6.1 Who received a 1.5kg package?
 select Recipient from Package where Weight = 1.5;
 
 select client.name 
@@ -14,7 +14,7 @@ WHERE Package.weight = 1.5;
 -- The result is "Al Gore's Head".
 
 
--- 7.2 What is the total weight of all the packages that he sent?
+-- 6.2 What is the total weight of all the packages that he sent?
 select sum(weight) from Package 
 where Sender = (
 select Recipient from Package where Weight = 1.5
