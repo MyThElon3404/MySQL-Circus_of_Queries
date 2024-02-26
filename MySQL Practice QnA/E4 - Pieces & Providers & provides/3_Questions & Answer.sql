1e4a2794d218615b3221d097a8a182af1e851751
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2024-02-26 12:08:53 +0530

    Update 3_Questions & Answer.sql

--- MySQL Practice QnA/E4 - Pieces & Providers & provides/3_Questions & Answer.sql
+++ MySQL Practice QnA/E4 - Pieces & Providers & provides/3_Questions & Answer.sql
@@ -1,177 +1,99 @@
--- 2.1 Select the last name of all employees.
-select LastName from Employees;
+show tables;
+
+select * from Pieces;
+select * from Providers;
+select * from provides; 
 
 
--- 2.2 Select the last name of all employees, without duplicates.
-select distinct LastName from employees;
+-- 5.1 Select the name of all the pieces. 
+select Name from Pieces;
 
+-- 5.2 Select all the providers' data. 
+select * from providers;
+ 
+-- 5.3 Obtain the average price of each piece (show only the piece code and the average price).
+select piece, avg(price) 
+from Provides 
+group by piece;
 
--- 2.3 Select all the data of employees whose last name is "Smith".
-select * from employees where lastname = 'Smith';
+-- 5.4 Obtain the names of all providers who supply piece 1.
+select Name 
+from Providers
+where Code in (
+	select  Provider from provides where Piece = 1
+); 
 
-
--- 2.4 Select all the data of employees whose last name is "Smith" or "Doe".
-select * from Employees where lastname in ('Smith', 'Doe');
-		-- OR
-select * from Employees where lastname = 'Smith' or lastname = 'Doe';
-
-
--- 2.5 Select all the data of employees that work in department 14.
-select * from Employees where department = 14;
-
-
--- 2.6 Select all the data of employees that work in department 37 or department 77.
-select * from employees where department = 37 or department = 77;
-			-- OR
-select * from employees where department in (37, 77);
-
-
--- 2.7 Select all the data of employees whose last name begins with an "S".
-select * from employees where LastName like 'S%';
-
--- 2.8 Select the sum of all the departments' budgets.
-select sum(budget) from Departments;
-		-- OR
-select Name, sum(Budget) from Departments group by Name;
-
-
--- 2.9 Select the number of employees in each department (you only need to show the department code and the number of employees).
-select Department, count(*) from employees group by department;
-
-
--- 2.10 Select all the data of employees, including each employee's department's data.
-select a.*, b.* from employees a join departments b on a.department = b.code;
-			-- OR
-SELECT SSN, E.Name AS Name_E, LastName, D.Name AS Name_D, Department, Code, Budget
-FROM Employees E INNER JOIN Departments D
-ON E.Department = D.Code;
-
-
--- 2.11 Select the name and last name of each employee, along with the name and budget of the employee's department.
-select a.name, a.lastname, b.name Department_name, b.Budget
-from employees a join departments b
-on a.department = b.code;
-
-
--- 2.12 Select the name and last name of employees working for departments with a budget greater than $60,000.
-select name, lastname
-from employees 
-where department in (
-	select code from departments where Budget>60000
-);
+select Providers.Name 
+from Providers join Provides
+on Providers.Code = Provides.Provider
+where Provides.Piece = 1;
 
 /* Without subquery */
-SELECT Employees.Name, LastName
-FROM Employees INNER JOIN Departments
-ON Employees.Department = Departments.Code
-AND Departments.Budget > 60000;
-
+ SELECT Providers.Name
+   FROM Providers INNER JOIN Provides
+          ON Providers.Code = Provides.Provider
+             AND Provides.Piece = 1;
+             
 /* With subquery */
-SELECT Name, LastName FROM Employees
-WHERE Department IN (
-	SELECT Code FROM Departments WHERE Budget > 60000
+SELECT Name
+FROM Providers
+WHERE Code IN (
+	SELECT Provider FROM Provides WHERE Piece = 1
 );
 
 
--- 2.13 Select the departments with a budget larger than the average budget of all the departments.
-select *
-from departments 
-where budget > (
-	select avg(budget) from departments
+-- 5.5 Select the name of pieces provided by provider with code "HAL".
+select Name from Pieces
+where Code in (
+select Piece from Provides where Provider = 'HAL'
 );
 
-/* With subquery */
-SELECT *
-FROM Departments
-WHERE Budget > (
-    SELECT AVG(Budget)
-    FROM Departments
-);
+select Pieces.Name
+from Pieces join Provides
+on (Pieces.code = Provides.Piece)
+where Provides.Provider = 'HAL';
 
-
--- 2.14 Select the names of departments with more than two employees.
-select b.name 
-from departments b
-where code in (
-select department
-from employees
-group by department
-having count(*)>2
-);
-
-/* With subquery */
-SELECT Name FROM Departments
-  WHERE Code IN (
-	SELECT Department
-	FROM Employees
-	GROUP BY Department
-	HAVING COUNT(*) > 2
+/* With EXISTS subquery */   
+-- Interesting clause
+SELECT Name
+  FROM Pieces
+  WHERE EXISTS(
+    SELECT * FROM Provides
+      WHERE Provider = 'HAL'
+        AND Piece = Pieces.Code
   );
 
-/* With UNION. This assumes that no two departments have
-   the same name */
-SELECT Departments.Name
-FROM Employees INNER JOIN Departments
-ON Department = Code
-GROUP BY Departments.Name
-HAVING COUNT(*) > 2;
+
+-- 5.6
+-- ---------------------------------------------
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- Intereting and important one.
+-- For each piece, find the most expensive offering of that piece and include the piece name, provider name, and price 
+-- (note that there could be two providers who supply the same piece at the most expensive price).
+
+SELECT Pieces.Name, Providers.Name, Price
+  FROM Pieces INNER JOIN Provides ON Pieces.Code = Piece
+              INNER JOIN Providers ON Providers.Code = Provider
+  WHERE Price =
+  (
+    SELECT MAX(Price) FROM Provides
+    WHERE Piece = Pieces.Code
+  );
+
+-- 5.7 Add an entry to the database to indicate that "Skellington Supplies" (code "TNBC") will provide sprockets (code "1") for 7 cents each.
+INSERT INTO Provides(Piece, Provider, Price) VALUES (1, 'TNBC', 7);
+
+-- 5.8 Increase all prices by one cent.
+UPDATE Provides
+SET Price = Price + 1;
+
+-- 5.9 Update the database to reflect that "Susan Calvin Corp." (code "RBT") will not supply bolts (code 4).
+DELETE FROM Provides WHERE provider = 'RBT' AND Piece = 4;
 
 
--- Very Important
--- 2.15 Select the name and last name of employees working for departments with second lowest budget.
-
-select name, lastname
-from employees
-where department =(
-select temp.code 
-from (
-	select * from departments order by budget limit 2) temp
-	order by temp.budget desc limit 1
-);
-
-
-/* With subquery */
-SELECT e.Name, e.LastName
-FROM Employees e 
-WHERE e.Department = (
-       SELECT sub.Code 
-       FROM (SELECT * FROM Departments d ORDER BY d.budget LIMIT 2) sub 
-       ORDER BY budget DESC LIMIT 1
-);
-       
-
--- 2.16
--- Add a new department called "Quality Assurance", with a budget of $40,000 and departmental code 11. 
--- Add an employee called "Mary Moore" in that department, with SSN 847-21-9811.
-insert into departments values(11, 'Quality Assurance', 40000);
-insert into employees values(847219811, 'Mary', 'Moore', 11);
-
-
--- 2.17 Reduce the budget of all departments by 10%.
-update departments 
-set budget = 0.9 * budget;
-
-
--- 2.18 Reassign all employees from the Research department (code 77) to the IT department (code 14).
-update employees
-set department = 14
-where department = 77;
-
--- 2.19 Delete from the table all employees in the IT department (code 14).
-
-delete from employees
-where department = 14;
-
--- 2.20 Delete from the table all employees who work in departments with a budget greater than or equal to $60,000.
-delete from employees
-where department in (
-select code 
-from departments
-where budget>=60000
-);
-
-
--- 2.21 Delete from the table all employees.
-delete from employees;
+-- 5.10 Update the database to reflect that "Susan Calvin Corp." (code "RBT") will not supply any pieces 
+-- (the provider should still remain in the database).
+DELETE FROM provides
+WHERE Provider = 'RBT';
 
 
