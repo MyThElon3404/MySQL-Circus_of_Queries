c5ba5751bed672a1d28036547ce3d93449741be8
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2024-02-27 11:08:49 +0530

    Update 1_Build Schema .sql

--- MySQL Practice QnA/E9 - Product and Manufacturer copy/1_Build Schema .sql
+++ MySQL Practice QnA/E9 - Product and Manufacturer copy/1_Build Schema .sql
@@ -1,41 +1,21 @@
--- Create tables -> Product & Manufacturer
--- ===============================================================================
+-- table PEOPLE: containing unique ID and corresponding names.
+CREATE TABLE PEOPLE (id INTEGER, name CHAR);
 
-drop table if EXISTS Manufacturers;
-CREATE TABLE Manufacturers (
-  Code INTEGER,
-  Name VARCHAR(255) NOT NULL,
-  PRIMARY KEY (Code)   
-);
+INSERT INTO PEOPLE VALUES(1, "A");
+INSERT INTO PEOPLE VALUES(2, "B");
+INSERT INTO PEOPLE VALUES(3, "C");
+INSERT INTO PEOPLE VALUES(4, "D");
 
-drop table if EXISTS Products;
-CREATE TABLE Products (
-  Code INTEGER,
-  Name VARCHAR(255) NOT NULL ,
-  Price DECIMAL NOT NULL,
-  Manufacturer INTEGER NOT NULL,
-  PRIMARY KEY (Code), 
-  FOREIGN KEY (Manufacturer) REFERENCES Manufacturers(Code)
-);
+-- ADDRESS: containing the history of address information of each ID.
+CREATE TABLE ADDRESS (id INTEGER, address CHAR, updatedate date);
 
-INSERT INTO Manufacturers(Code,Name) VALUES(1,'Sony');
-INSERT INTO Manufacturers(Code,Name) VALUES(2,'Creative Labs');
-INSERT INTO Manufacturers(Code,Name) VALUES(3,'Hewlett-Packard');
-INSERT INTO Manufacturers(Code,Name) VALUES(4,'Iomega');
-INSERT INTO Manufacturers(Code,Name) VALUES(5,'Fujitsu');
-INSERT INTO Manufacturers(Code,Name) VALUES(6,'Winchester');
-
-INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(1,'Hard drive',240,5);
-INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(2,'Memory',120,6);
-INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(3,'ZIP drive',150,4);
-INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(4,'Floppy disk',5,6);
-INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(5,'Monitor',240,1);
-INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(6,'DVD drive',180,2);
-INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(7,'CD drive',90,2);
-INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(8,'Printer',270,3);
-INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(9,'Toner cartridge',66,3);
-INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(10,'DVD burner',180,2);
-
-
-
-
+INSERT INTO ADDRESS VALUES(1, "address-1-1", "2016-01-01");
+INSERT INTO ADDRESS VALUES(1, "address-1-2", "2016-09-02");
+INSERT INTO ADDRESS VALUES(2, "address-2-1", "2015-11-01");
+INSERT INTO ADDRESS VALUES(3, "address-3-1", "2016-12-01");
+INSERT INTO ADDRESS VALUES(3, "address-3-2", "2014-09-11");
+INSERT INTO ADDRESS VALUES(3, "address-3-3", "2015-01-01");
+INSERT INTO ADDRESS VALUES(4, "address-4-1", "2010-05-21");
+INSERT INTO ADDRESS VALUES(4, "address-4-2", "2012-02-11");
+INSERT INTO ADDRESS VALUES(4, "address-4-3", "2015-04-27");
+INSERT INTO ADDRESS VALUES(4, "address-4-4", "2014-01-01");
