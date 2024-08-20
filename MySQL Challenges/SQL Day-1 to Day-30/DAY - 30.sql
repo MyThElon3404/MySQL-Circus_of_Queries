e788b1ee6cceccdda06559892c8ad9d76d5d57d3
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2024-08-21 00:01:51 +0530

    Update DAY - 30.sql

--- MySQL Challenges/SQL Day-1 to Day-30/DAY - 30.sql
+++ MySQL Challenges/SQL Day-1 to Day-30/DAY - 30.sql
@@ -1,6 +1,8 @@
 -- QUESTION : 1
--- 1. CVS Health is trying to better understand its pharmacy sales, and how well different products are selling. Each drug can only be produced by one manufacturer.
--- Write a query to find the top 3 most profitable drugs sold, and how much profit they made. Assume that there are no ties in the profits. ----- Display the result from the highest to the lowest total profit.
+-- 1. CVS Health is trying to better understand its pharmacy sales, and how well different products are selling. 
+-- Each drug can only be produced by one manufacturer.
+-- Write a query to find the top 3 most profitable drugs sold, and how much profit they made. Assume that there are no ties in the profits.
+-- Display the result from the highest to the lowest total profit.
 
 CREATE TABLE pharmacy_sales (
     product_id INTEGER,
@@ -10,6 +12,7 @@ CREATE TABLE pharmacy_sales (
     manufacturer VARCHAR(255),
     drug VARCHAR(255)
 );
+
 INSERT INTO pharmacy_sales (product_id, units_sold, total_sales, cogs, manufacturer, drug)
 VALUES
 (9, 37410, 293452.54, 208876.01, 'Eli Lilly', 'Zyprexa'),
@@ -28,7 +31,9 @@ LIMIT 3; -- TOP 3
 -- ==================================================================================================================================
 
 -- QUESTION : 2
--- 2. UnitedHealth Group (UHG) has a program called Advocate4Me, which allows policy holders (or, members) to call an advocate and receive support for their health care needs – whether that's claims and benefits support, drug coverage, pre- and post-authorisation, medical records, emergency assistance, or member portal services.
+-- 2. UnitedHealth Group (UHG) has a program called Advocate4Me, which allows policy holders (or, members) to call an advocate 
+-- and receive support for their health care needs – whether that's claims and benefits support, drug coverage, pre- and post-authorisation, 
+-- medical records, emergency assistance, or member portal services.
 -- Write a query to find how many UHG policy holders made three, or more calls, assuming each call is identified by the case_id column.
 
 CREATE TABLE callers (
@@ -38,7 +43,9 @@ CREATE TABLE callers (
     call_date TIMESTAMP,
     call_duration_secs INTEGER
 );
-INSERT INTO callers (policy_holder_id, case_id, call_category, call_date, call_duration_secs) VALUES
+
+INSERT INTO callers (policy_holder_id, case_id, call_category, call_date, call_duration_secs) 
+VALUES
 (1, 'f1d012f9-9d02-4966-a968-bf6c5bc9a9fe', 'emergency assistance', '2023-04-13T19:16:53Z', 144),
 (1, '41ce8fb6-1ddd-4f50-ac31-07bfcce6aaab', 'authorisation', '2023-05-25T09:09:30Z', 815),
 (2, '9b1af84b-eedb-4c21-9730-6f099cc2cc5e', 'claims assistance', '2023-01-26T01:21:27Z', 992),
