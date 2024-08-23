7e2264d6b81d6090a4a9d2229980b80aa04dbaac
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2024-08-23 11:13:52 +0530

    Update DAY - 31.sql

--- MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 31.sql
+++ MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 31.sql
@@ -44,44 +44,39 @@ group by loan_id;
 -- ==================================================================================================================================
 
 -- QUESTION : 2
--- 2. Determine the users who have posted more than 2 times in the past week and calculate the total number of likes
--- they have received. Return user_id and number of post and no of likes
+-- 2. Write a query that returns the rate_type, loan_id, loan balance , and a column that shows with 
+-- what percentage the loan's balance contributes to the total balance among the loans of the same rate type.
 
-drop table if exists posts;
-CREATE TABLE posts (
-    post_id INT PRIMARY KEY,
-    user_id INT,
-    likes INT,
-    post_date DATE
+drop table if exists submissions;
+CREATE TABLE submissions (
+    id INT PRIMARY KEY,
+    balance FLOAT,
+    interest_rate FLOAT,
+    rate_type VARCHAR(10),
+    loan_id INT
 );
-INSERT INTO posts (post_id, user_id, likes, post_date) 
+INSERT INTO submissions (id, balance, interest_rate, rate_type, loan_id)
 VALUES
-	(1, 101, 50, '2024-02-27'),	(2, 102, 30, '2024-02-28'),
-	(3, 103, 70, '2024-02-29'), (4, 101, 80, '2024-02-01'),
-	(5, 102, 40, '2024-02-02'), (6, 103, 60, '2024-02-29'),
-	(7, 101, 90, '2024-01-29'), (8, 101, 20, '2024-02-05'),
-	(9, 102, 50, '2024-01-29'), (10, 103, 30, '2024-02-29'),
-	(11, 101, 60, '2024-01-08'), (12, 102, 70, '2024-01-09'),
-	(13, 103, 80, '2024-01-10'), (14, 101, 40, '2024-01-29'),
-	(15, 102, 90, '2024-01-29'), (16, 103, 20, '2024-01-13'),
-	(17, 101, 70, '2024-01-14'), (18, 102, 50, '2024-02-29'),
-	(19, 103, 30, '2024-02-16'), (20, 101, 60, '2024-02-17');
+(1, 5229.12, 8.75, 'variable', 2),
+(2, 12727.52, 11.37, 'fixed', 4),
+(3, 14996.58, 8.25, 'fixed', 9),
+(4, 21149, 4.75, 'variable', 7),
+(5, 14379, 3.75, 'variable', 5),
+(6, 6221.12, 6.75, 'variable', 11);
 
-select * from posts;
+select * from submissions;
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
-with current_date_cte as (
-	select min(post_date) as first_date
-	from posts
+with sub_cte as (
+	select *,
+		sum(balance) over(partition by rate_type) as total_balance
+	from submissions
 )
-select user_id,
-	count(post_id) as total_post,
-	sum(likes) as total_likes
-from posts
-where post_date >= (select first_date from current_date_cte)
-	and post_date <= DATEADD(DAY, 7, (select first_date from current_date_cte))
-group by user_id
-having count(post_id) >= 2;
+select rate_type, loan_id,
+	balance as loan_balance,
+	total_balance,
+	ROUND((balance / total_balance) * 100, 2) as percentage_contribution
+from sub_cte;
 
 -- ==================================================================================================================================
