d11abaac25492a4a265a5e5d2de94e6c6ffa2d26
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2024-11-15 00:03:18 +0530

    Update DAY - 37.sql

--- MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 37.sql
+++ MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 37.sql
@@ -1,5 +1,5 @@
 -- QUESTION : 1
--- 1. Write a query to replace rent at null value
+-- 1. Write a query to replace rent value at null value
 
 create table tb (
 	id int,
@@ -53,39 +53,23 @@ FROM cte;
 -- ==================================================================================================================================
 
 -- QUESTION : 2
--- 2. Write a query that returns the rate_type, loan_id, loan balance , and a column that shows with 
--- what percentage the loan's balance contributes to the total balance among the loans of the same rate type.
+-- 2. Write a query to replace brands value at null value
 
-drop table if exists submissions;
-CREATE TABLE submissions (
-    id INT PRIMARY KEY,
-    balance FLOAT,
-    interest_rate FLOAT,
-    rate_type VARCHAR(10),
-    loan_id INT
+create table brands (
+category varchar(20),
+brand_name varchar(20)
 );
-INSERT INTO submissions (id, balance, interest_rate, rate_type, loan_id)
-VALUES
-(1, 5229.12, 8.75, 'variable', 2),
-(2, 12727.52, 11.37, 'fixed', 4),
-(3, 14996.58, 8.25, 'fixed', 9),
-(4, 21149, 4.75, 'variable', 7),
-(5, 14379, 3.75, 'variable', 5),
-(6, 6221.12, 6.75, 'variable', 11);
-
-select * from submissions;
+insert into brands 
+values ('chocolates','5-star')
+,(null,'dairy milk')
+,(null,'perk')
+,(null,'eclair')
+,('Biscuits','britannia')
+,(null,'good day')
+,(null,'boost');
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
-with sub_cte as (
-	select *,
-		sum(balance) over(partition by rate_type) as total_balance
-	from submissions
-)
-select rate_type, loan_id,
-	balance as loan_balance,
-	total_balance,
-	ROUND((balance / total_balance) * 100, 2) as percentage_contribution
-from sub_cte;
+
 
 -- ==================================================================================================================================
