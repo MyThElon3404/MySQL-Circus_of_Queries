762d33032c9d18b124900f1c80672a50df61c8bf
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2024-11-16 20:45:24 +0530

    Update DAY - 39.sql

--- MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 39.sql
+++ MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 39.sql
@@ -1,9 +1,60 @@
 -- QUESTION : 1
--- 1. 
+-- 1. Assume you're given tables with information on Snapchat users, including their ages and time spent sending and opening snaps. 
+-- Write an SQL query to obtain a breakdown of the time spent sending vs. opening snaps as a percentage of total time spent on these activities,
+-- grouped by age group. Round the percentages to 2 decimal places.
+
+-- 𝑪𝒂𝒍𝒄𝒖𝒍𝒂𝒕𝒆 𝒕𝒉𝒆 𝒇𝒐𝒍𝒍𝒐𝒘𝒊𝒏𝒈 𝒑𝒆𝒓𝒄𝒆𝒏𝒕𝒂𝒈𝒆𝒔:
+
+-- Time spent sending / (Time spent sending + Time spent opening)
+-- Time spent opening / (Time spent sending + Time spent opening)
+-- To avoid integer division in percentages, multiply by 100.0 and not 100.
+
+DROP TABLE IF EXISTS activities;
+CREATE TABLE activities (
+ activity_id INTEGER,
+ user_id INTEGER,
+ activity_type VARCHAR(6), -- 'send', 'open', 'chat'
+ time_spent FLOAT,
+ activity_date DATETIME
+);
+INSERT INTO activities (activity_id, user_id, activity_type, time_spent, activity_date) 
+VALUES
+(7274, 123, 'open', 4.50, '2022-06-22 12:00:00'),
+(2425, 123, 'send', 3.50, '2022-06-22 12:00:00'),
+(1413, 456, 'send', 5.67, '2022-06-23 12:00:00'),
+(1414, 789, 'chat', 11.00, '2022-06-25 12:00:00'),
+(2536, 456, 'open', 3.00, '2022-06-25 12:00:00');
+
+
+DROP TABLE IF EXISTS age_breakdown;
+CREATE TABLE age_breakdown (
+ user_id INTEGER,
+ age_bucket VARCHAR(6) -- e.g., '21-25', '26-30', '31-35'
+);
+INSERT INTO age_breakdown (user_id, age_bucket) VALUES
+(123, '31-35'),
+(456, '26-30'),
+(789, '21-25');
+
+select * from activities;
+select * from age_breakdown;
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
-
+with send_open_spent_time as (
+	select ab.age_bucket as age_group,
+		sum(case when activity_type='send' then time_spent end) as send_spent_time,
+		sum(case when activity_type='open' then time_spent end) as open_spent_time
+	from activities as att
+	join age_breakdown as ab
+		on att.user_id = ab.user_id
+	where att.activity_type in ('send', 'open')
+	group by ab.age_bucket
+)
+select age_group,
+	round((send_spent_time)*100.0 / (send_spent_time+open_spent_time), 2) as send_perc,
+	round((open_spent_time)*100.0 / (send_spent_time+open_spent_time), 2) as open_perc
+from send_open_spent_time;
 
 -- ==================================================================================================================================
 
