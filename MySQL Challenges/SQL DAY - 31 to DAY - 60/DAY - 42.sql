97078dd25a4ca8b0fdd28b02cb5e62e868c55f5e
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2025-01-07 11:08:47 +0530

    Update DAY - 42.sql

--- MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 42.sql
+++ MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 42.sql
@@ -57,9 +57,59 @@ FROM WindowedData;
 -- ==================================================================================================================================
 
 -- QUESTION : 2 
+-- 2. You are given a table called EmployeeIncentives with columns: Name, Age, Incentive Band, and Amount.
+-- 🧩 Question:Find the Name, Age, Incentive Band, and Amount from the EmployeeIncentives table, 
+-- calculate the minimum incentive for each Name and Age, 
+-- and the maximum incentive for Incentive Band 1 for each Name and Age, sorted by Name, Age, and Incentive Band.
+
+DROP TABLE IF EXISTS EmployeeIncentives;
+CREATE TABLE EmployeeIncentives (
+	Name VARCHAR(50), 
+	Age INT, 
+	IncentiveBand INT, 
+	Amount INT);
+
+INSERT INTO EmployeeIncentives (Name, Age, IncentiveBand, Amount) 
+	VALUES 
+('John Doe', 30, 1, 150), ('John Doe', 30, 2, 250), 
+('Jane Smith', 25, 1, 300), ('Jane Smith', 25, 3, 450), 
+('Alice Brown', 35, 1, 180), ('Alice Brown', 35, 2, 350);
 
 -- 2. -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
+-- SOLUTION 1 - with case and basic SQL statements
+select Name, Age, IncentiveBand, Amount,
+	min(Amount) over(partition by Name, Age) as MinIncentive,
+	max(
+		case
+			when IncentiveBand = 1 then Amount 
+			else NULL
+		end
+	) over (partition by Name, Age) as Band1Incentive
+from EmployeeIncentives;
 
+-- SOLUTION 2 - Use a CTE or subquery to calculate the required fields
+WITH MinIncentives AS (
+    SELECT Name, Age,
+        MIN(Amount) AS MinIncentive
+    FROM EmployeeIncentives
+    GROUP BY Name, Age
+),
+MaxIncentivesBand1 AS (
+    SELECT Name, Age,
+        MAX(Amount) AS MaxIncentiveBand1
+    FROM EmployeeIncentives
+    WHERE IncentiveBand = 1
+    GROUP BY Name, Age
+)
+SELECT EI.Name,EI.Age, EI.IncentiveBand,
+    EI.Amount, MI.MinIncentive,
+    COALESCE(MB1.MaxIncentiveBand1, 0) AS MaxIncentiveBand1
+FROM EmployeeIncentives EI
+LEFT JOIN MinIncentives MI
+    ON EI.Name = MI.Name AND EI.Age = MI.Age
+LEFT JOIN MaxIncentivesBand1 MB1
+    ON EI.Name = MB1.Name AND EI.Age = MB1.Age
+ORDER BY EI.Name, EI.Age, EI.IncentiveBand;
 
 -- ==================================================================================================================================
