acc1ad9d1689007c5c793610404a6dfe3fcc79a5
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2025-01-23 11:42:18 +0530

    Update DAY - 45.sql

--- MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 45.sql
+++ MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 45.sql
@@ -1,11 +1,36 @@
 -- QUESTION : 1
--- 1. 
+-- 1. Write an SQL query to report the difference between number of apples and oranges sold each day.
+-- Return the result table ordered by sale_date in format ('YYYY-MM-DD').
 
+CREATE TABLE Sales (
+    sale_date DATE NOT NULL,
+    fruit NVARCHAR(10) NOT NULL CHECK (fruit IN ('apples', 'oranges')),
+    sold_num INT NOT NULL,
+    PRIMARY KEY (sale_date, fruit)
+);
 
+INSERT INTO Sales (sale_date, fruit, sold_num) VALUES
+('2020-05-01', 'apples', 10),
+('2020-05-01', 'oranges', 8),
+('2020-05-02', 'apples', 15),
+('2020-05-02', 'oranges', 15),
+('2020-05-03', 'apples', 20),
+('2020-05-03', 'oranges', 0),
+('2020-05-04', 'apples', 15),
+('2020-05-04', 'oranges', 16);
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
-
+select sale_date,
+	sum(case
+		when fruit = 'apples' then sold_num else 0
+	end) -
+	sum(case
+		when fruit = 'oranges' then sold_num else 0
+	end) as apples_oranges_diff
+from sales
+group by sale_date
+order by sale_date;
 
 -- ==================================================================================================================================
 
