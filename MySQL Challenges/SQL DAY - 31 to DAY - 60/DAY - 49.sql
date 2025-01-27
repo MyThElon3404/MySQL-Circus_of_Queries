e16146c8ffe9516cdafc78cb497b0d835f26a51b
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2025-01-27 22:24:08 +0530

    Update DAY - 49.sql

--- MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 49.sql
+++ MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 49.sql
@@ -34,12 +34,45 @@ having count(distinct c.product_key) =
 -- ==================================================================================================================================
 
 -- QUESTION : 2
--- 2. 
+-- 2. -- Write a SQL query to find employees who have the highest salary in each of the departments.
 
+CREATE TABLE Department (
+    Id INT PRIMARY KEY,
+    Name VARCHAR(50)
+);
+INSERT INTO Department (Id, Name)
+VALUES 
+    (1, 'IT'),
+    (2, 'Sales');
 
+CREATE TABLE Employee (
+    Id INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Salary INT,
+    DepartmentId INT,
+    FOREIGN KEY (DepartmentId) REFERENCES Department(Id)
+);
+INSERT INTO Employee (Id, Name, Salary, DepartmentId)
+VALUES
+    (1, 'Joe', 70000, 1),
+    (2, 'Jim', 90000, 1),
+    (3, 'Henry', 80000, 2),
+    (4, 'Sam', 60000, 2),
+    (5, 'Max', 90000, 1);
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
-
+with emp_dept_cte as (
+	select d.name as dept_name, e.name as emp_name,
+		e.salary,
+		DENSE_RANK() over(partition by d.name order by e.salary desc) drn
+	from Employee e
+	inner join Department d
+		on e.DepartmentId = d.Id
+)
+select dept_name, emp_name,
+	salary
+from emp_dept_cte
+where drn = 1;
 
 -- ==================================================================================================================================
