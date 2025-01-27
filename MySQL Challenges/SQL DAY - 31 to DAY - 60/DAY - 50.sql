eb65a307e6cf1d56d5d3132a357e47d3463344b2
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2025-01-27 23:41:20 +0530

    Update DAY - 50.sql

--- MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 50.sql
+++ MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 50.sql
@@ -57,12 +57,72 @@ JOIN Variables v2 ON e.right_operand = v2.name;
 -- ==================================================================================================================================
 
 -- QUESTION : 2
--- 2. 
+-- 2. Mary wants to change seats for the adjacent students.
+-- Can you write a SQL query to output the result for Mary?
+ 
+-- +---------+---------+
+-- |    id   | student |
+-- +---------+---------+
+-- |    1    | Abbot   |
+-- |    2    | Doris   |
+-- |    3    | Emerson |
+-- |    4    | Green   |
+-- |    5    | Jeames  |
+-- +---------+---------+
+-- For the sample input, the output is:
+-- +---------+---------+
+-- |    id   | student |
+-- +---------+---------+
+-- |    1    | Doris   |
+-- |    2    | Abbot   |
+-- |    3    | Green   |
+-- |    4    | Emerson |
+-- |    5    | Jeames  |
+-- +---------+---------+
 
-
+CREATE TABLE Students (
+    id INT PRIMARY KEY,
+    student VARCHAR(50)
+);
+INSERT INTO Students (id, student)
+VALUES
+    (1, 'Abbot'),
+    (2, 'Doris'),
+    (3, 'Emerson'),
+    (4, 'Green'),
+    (5, 'Jeames');
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
+WITH SwappedStudents AS (
+    SELECT 
+        id, 
+        student, 
+        ROW_NUMBER() OVER (ORDER BY id) AS row_num
+    FROM Students
+)
+SELECT 
+    CASE
+        WHEN s1.row_num % 2 = 1 THEN s2.id
+        ELSE s1.id
+    END AS id,
+    CASE
+        WHEN s1.row_num % 2 = 1 THEN s2.student
+        ELSE s1.student
+    END AS student
+FROM SwappedStudents s1
+JOIN SwappedStudents s2 
+    ON s1.row_num = s2.row_num - 1
+ORDER BY id;
 
+------------------------------------------- OR --------------------------------------------------
+
+SELECT 
+    ROW_NUMBER() OVER (ORDER BY CASE 
+                                    WHEN id % 2 = 1 THEN id + 1
+                                    ELSE id - 1
+                                 END) AS id,
+    student
+FROM Students;
 
 -- ==================================================================================================================================
