c89db8aa3f7a0e617d1e11f48736dab6362b3062
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2025-01-28 18:05:10 +0530

    Update DAY - 51.sql

--- MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 51.sql
+++ MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 51.sql
@@ -1,45 +1,26 @@
 -- QUESTION : 1
--- 1. Write a query that returns binary description of rate type per loan_id. 
--- The results should have one row per loan_id and two columns: for fixed and variable type.
+-- 1. Since some IDs have been removed from Logs. 
+-- Write an SQL query to find the start and end number of continuous ranges in table Logs.
+-- Order the result table by start_id.
 
-drop table if exists submissions;
-CREATE TABLE submissions (
-    id INT PRIMARY KEY,
-    balance FLOAT,
-    interest_rate FLOAT,
-    rate_type VARCHAR(10),
-    loan_id INT
+CREATE TABLE Logs (
+    log_id INT PRIMARY KEY
 );
-INSERT INTO submissions (id, balance, interest_rate, rate_type, loan_id)
-VALUES
-(1, 5229.12, 8.75, 'variable', 2),
-(2, 12727.52, 11.37, 'fixed', 4),
-(3, 14996.58, 8.25, 'fixed', 9),
-(4, 21149, 4.75, 'variable', 7),
-(5, 14379, 3.75, 'variable', 5),
-(6, 6221.12, 6.75, 'variable', 11),
-(7, 5229.12, 8.75, 'fixed', 2),
-(8, 12727.52, 11.37, 'variable', 4),
-(9, 14996.58, 8.25, 'fixed', 9),
-(10, 21149, 4.75, 'fixed', 7),
-(11, 14379, 3.75, 'variable', 5),
-(12, 6221.12, 6.75, 'fixed', 11);
-
-select * from submissions;
+INSERT INTO Logs (log_id) 
+VALUES 
+(1), (2), (3), (7), (8), (10);
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
-select loan_id,
-	MAX(case
-		when rate_type='fixed' then 1
-		else 0
-	end) as fixed_rate,
-	MAX(case
-		when rate_type='variable' then 1
-		else 0
-	end) as variable_rate
-from submissions
-group by loan_id;
+with cte as (
+  select log_id,
+  log_id - row_number() over(order by log_id) as rn
+  from Logs
+)
+select min(log_id) as start_num,
+  max(log_id) as end_num
+from cte
+group by rn;
 
 -- ==================================================================================================================================
 
