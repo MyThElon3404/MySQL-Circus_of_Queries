00a1d63219ae697d257b95d0635811e7d90b1c8e
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2025-02-26 11:50:23 +0530

    Update DAY - 54.sql

--- MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 54.sql
+++ MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 54.sql
@@ -40,10 +40,51 @@ where model_id not in (
 -- QUESTION : 2
 -- 2. 
 
+create table hospital ( 
+	emp_id int, 
+	action varchar(10), 
+	time datetime
+);
 
+insert into hospital values ('1', 'in', '2019-12-22 09:00:00');
+insert into hospital values ('1', 'out', '2019-12-22 09:15:00');
+insert into hospital values ('2', 'in', '2019-12-22 09:00:00');
+insert into hospital values ('2', 'out', '2019-12-22 09:15:00');
+insert into hospital values ('2', 'in', '2019-12-22 09:30:00');
+insert into hospital values ('3', 'out', '2019-12-22 09:00:00');
+insert into hospital values ('3', 'in', '2019-12-22 09:15:00');
+insert into hospital values ('3', 'out', '2019-12-22 09:30:00');
+insert into hospital values ('3', 'in', '2019-12-22 09:45:00');
+insert into hospital values ('4', 'in', '2019-12-22 09:45:00');
+insert into hospital values ('5', 'out', '2019-12-22 09:40:00');
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
+-- SOLUTION 1: 
+with cte as (
+	select emp_id,
+		count(case when action = 'in' then 1 end) as emp_in_cnt,
+		count(case when action = 'out' then 1 end) as emp_out_cnt
+	from hospital
+	group by emp_id
+	having count(case when action = 'in' then 1 end) > 0
+)
+select emp_id
+from cte
+where emp_in_cnt != emp_out_cnt;
 
+-- SOLUTION 2: 
+with cte as (
+	select emp_id,
+		max(case when action = 'in' then time end) as max_in_time,
+		max(case when action = 'out' then time end) as max_out_time
+	from hospital
+	group by emp_id
+	having max(case when action = 'in' then time end) is not null
+)
+select emp_id
+from cte
+where max_in_time > max_out_time 
+	or max_out_time is null;
 
 -- ==================================================================================================================================
