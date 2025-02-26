064a6ca8ddeb9edfaaba63c637cc8c0339de932a
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2025-02-26 12:29:02 +0530

    Update DAY - 56.sql

--- MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 56.sql
+++ MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 56.sql
@@ -34,15 +34,42 @@ order by num_time_searched desc;
 -- ==================================================================================================================================
 
 -- QUESTION : 2
--- 2. 
+-- 2. Person and type is the column names and it is the input table
+-- We need output as pair table
+-- Like these are adult and child in the table and they are going for a fair and they have a ride on some jhoola, 
+-- so one adult can go with one child and is last one adult will be alone
 
+create table family (
+	person varchar(5),
+	type varchar(10),
+	age int
+);
 
+insert into family 
+values 
+('A1','Adult',54),('A2','Adult',53),
+('A3','Adult',52),('A4','Adult',58),
+('A5','Adult',54),('C1','Child',20),
+('C2','Child',19),('C3','Child',22),
+('C4','Child',15);
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
--- SOLUTION 1: 
-
-
--- SOLUTION 2: 
+with adult_cte as (
+	SELECT Person AS Adult, 
+		ROW_NUMBER() OVER (ORDER BY Person) AS RowNum
+	FROM family
+	WHERE Type = 'Adult'
+),
+	child_cte as (
+		SELECT Person AS Child, 
+			ROW_NUMBER() OVER (ORDER BY Person) AS RowNum
+		FROM family
+		WHERE Type = 'Child'
+	)
+select a.Adult, COALESCE(c.Child, '') as Child 
+from adult_cte a
+left join child_cte c
+	on a.RowNum = c.RowNum;
 
 -- ==================================================================================================================================
