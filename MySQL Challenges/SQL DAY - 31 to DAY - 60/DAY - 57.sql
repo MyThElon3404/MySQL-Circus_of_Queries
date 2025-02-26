7920110856bd49d2a8f0263b16d87ebfe86b1a58
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2025-02-26 12:55:57 +0530

    Update DAY - 57.sql

--- MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 57.sql
+++ MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 57.sql
@@ -59,12 +59,55 @@ ORDER BY tc DESC;
 -- ==================================================================================================================================
 
 -- QUESTION : 2
--- 2. 
+-- 2. write a SQL query to find out supplier_id, product_id, and starting date of record_date 
+-- for which stock quantity is less than 50 for two or more consecutive days.
 
+CREATE TABLE stock (
+    supplier_id INT,
+    product_id INT,
+    stock_quantity INT,
+    record_date DATE
+);
+
+INSERT INTO stock (supplier_id, product_id, stock_quantity, record_date)
+VALUES
+    (1, 1, 60, '2022-01-01'),
+    (1, 1, 40, '2022-01-02'),
+    (1, 1, 35, '2022-01-03'),
+    (1, 1, 45, '2022-01-04'),
+    (1, 1, 51, '2022-01-06'),
+    (1, 1, 55, '2022-01-09'),
+    (1, 1, 25, '2022-01-10'),
+    (1, 1, 48, '2022-01-11'),
+    (1, 1, 45, '2022-01-15'),
+    (1, 1, 38, '2022-01-16'),
+    (1, 2, 45, '2022-01-08'),
+    (1, 2, 40, '2022-01-09'),
+    (2, 1, 45, '2022-01-06'),
+    (2, 1, 55, '2022-01-07'),
+    (2, 2, 45, '2022-01-08'),
+    (2, 2, 48, '2022-01-09'),
+    (2, 2, 35, '2022-01-10'),
+    (2, 2, 52, '2022-01-15'),
+    (2, 2, 23, '2022-01-16');
 
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
+WITH stock_cte AS (
+    SELECT supplier_id, product_id, 
+        stock_quantity, record_date,
+        LAG(stock_quantity) OVER (PARTITION BY supplier_id, product_id ORDER BY record_date) AS prev_stock,
+        LAG(record_date) OVER (PARTITION BY supplier_id, product_id ORDER BY record_date) AS prev_date
+    FROM stock
+)
+SELECT supplier_id, product_id, 
+    MIN(record_date) AS start_date
+FROM stock_cte
+WHERE stock_quantity < 50 
+  AND prev_stock < 50 
+  AND DATEDIFF(day, prev_date, record_date) = 1  -- Ensures consecutive days
+GROUP BY supplier_id, product_id;
 
 
 -- ==================================================================================================================================
