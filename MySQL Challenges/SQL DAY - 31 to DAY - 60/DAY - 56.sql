fa7f75dc3550c6390fd80b2ae7231c1bdd18fc85
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2025-02-26 12:03:48 +0530

    Update DAY - 56.sql

--- MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 56.sql
+++ MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 56.sql
@@ -1,14 +1,35 @@
 -- QUESTION : 1
--- 1. 
+-- 1. The problem is called as Most Popular Room Types.  In this problem we will learn how to convert comma separated values into row.
+-- find the most popular room type. output with room type and no. of time it searches.
 
+create table airbnb_searches (
+	user_id int,
+	date_searched date,
+	filter_room_types varchar(200)
+);
+
+insert into airbnb_searches 
+values
+(1,'2022-01-01','entire home,private room')
+,(2,'2022-01-02','entire home,shared room')
+,(3,'2022-01-02','private room,shared room')
+,(4,'2022-01-03','private room');
 
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
 -- SOLUTION 1: 
-
--- SOLUTION 2: 
-
+with cte as (
+	select user_id, date_searched,
+		TRIM(value) as room_type
+	from airbnb_searches
+	cross apply string_split(filter_room_types, ',')
+)
+select room_type,
+	count(*) as num_time_searched
+from cte
+group by room_type
+order by num_time_searched desc;
 
 -- ==================================================================================================================================
 
