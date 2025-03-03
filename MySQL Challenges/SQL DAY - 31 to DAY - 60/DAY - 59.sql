59e1b55bc85066d30fe42a730057b4a2e2a79d99
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2025-03-04 00:13:57 +0530

    Update DAY - 59.sql

--- MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 59.sql
+++ MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 59.sql
@@ -1,6 +1,24 @@
 -- QUESTION : 1
 -- 1. 
 
+drop table if exists entries;
+CREATE TABLE entries (
+    name VARCHAR(20),
+    address VARCHAR(20),
+    email VARCHAR(50),
+    floor INT,
+    resources VARCHAR(10)
+);
+
+INSERT INTO entries 
+	(name, address, email, floor, resources) 
+VALUES 
+    ('A', 'Bangalore', 'A@gmail.com', 1, 'CPU'),
+    ('A', 'Bangalore', 'A1@gmail.com', 1, 'CPU'),
+    ('A', 'Bangalore', 'A2@gmail.com', 2, 'DESKTOP'),
+    ('B', 'Bangalore', 'B@gmail.com', 2, 'DESKTOP'),
+    ('B', 'Bangalore', 'B1@gmail.com', 2, 'DESKTOP'),
+    ('B', 'Bangalore', 'B2@gmail.com', 1, 'MONITOR');
 
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
