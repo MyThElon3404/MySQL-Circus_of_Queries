24f8fda774a26d204323ab987bd218e7cdbeb313
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2025-03-05 00:45:19 +0530

    Update DAY - 59.sql

--- MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 59.sql
+++ MySQL Challenges/SQL DAY - 31 to DAY - 60/DAY - 59.sql
@@ -51,12 +51,57 @@ group by month;
 -- ==================================================================================================================================
 
 -- QUESTION : 2
--- 2. 
+-- 2. Assume you're given a table containing information about Wayfair user transactions for different products. 
+-- Write a query to calculate the year-on-year growth rate for the total spend of each product, grouping the results by product ID.
+
+-- The output should include the year in ascending order, product ID, current year's spend, 
+-- previous year's spend and year-on-year growth percentage, rounded to 2 decimal places.
 
 
+DROP TABLE IF EXISTS user_transactions;
+CREATE TABLE user_transactions (
+    transaction_id INT PRIMARY KEY,
+    product_id INT,
+    spend DECIMAL(10,2),
+    transaction_date DATETIME
+);
+
+INSERT INTO user_transactions (transaction_id, product_id, spend, transaction_date) 
+	VALUES
+(1341, 123424, 1500.60, '2019-12-31 12:00:00'),
+(1423, 123424, 1000.20, '2020-12-31 12:00:00'),
+(1623, 123424, 1246.44, '2021-12-31 12:00:00'),
+(1322, 123424, 2145.32, '2022-12-31 12:00:00'),
+(1344, 234412, 1800.00, '2019-12-31 12:00:00'),
+(1435, 234412, 1234.00, '2020-12-31 12:00:00'),
+(4325, 234412, 889.50, '2021-12-31 12:00:00'),
+(5233, 234412, 2900.00, '2022-12-31 12:00:00'),
+(2134, 543623, 6450.00, '2019-12-31 12:00:00'),
+(1234, 543623, 5348.12, '2020-12-31 12:00:00'),
+(2423, 543623, 2345.00, '2021-12-31 12:00:00'),
+(1245, 543623, 5680.00, '2022-12-31 12:00:00');
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
-
+WITH current_spend_cte AS (
+    SELECT 
+        YEAR(transaction_date) AS years, 
+        product_id,
+        SUM(spend) AS current_year_spend
+    FROM user_transactions
+    GROUP BY YEAR(transaction_date), product_id
+),
+prev_spend_cte AS (
+    SELECT *,
+        LAG(current_year_spend) OVER (PARTITION BY product_id ORDER BY years) AS prev_year_spend
+    FROM current_spend_cte
+)
+SELECT *,
+    CASE 
+        WHEN prev_year_spend IS NULL THEN NULL  -- NULL means no previous data
+        ELSE ROUND(((current_year_spend - prev_year_spend) / prev_year_spend) * 100, 2) 
+    END AS YOY_Growth_Rate_Per_Product
+FROM prev_spend_cte
+ORDER BY product_id, years;
 
 -- ==================================================================================================================================
