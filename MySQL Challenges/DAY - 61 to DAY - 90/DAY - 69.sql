4b01cf7b98d14457ae8a4d5e892557d2d0637e29
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2025-04-03 00:30:41 +0530

    Update DAY - 69.sql

--- MySQL Challenges/DAY - 61 to DAY - 90/DAY - 69.sql
+++ MySQL Challenges/DAY - 61 to DAY - 90/DAY - 69.sql
@@ -81,13 +81,47 @@ group by user_id;
 -- ==================================================================================================================================
 
 -- QUESTION : 2
--- 2. 
+-- 2. Find IDs of LinkedIn users who were employed at Google on November 1st, 2021. 
+-- Do not consider users who started or ended their employment at Google on that day 
+-- but do include users who changed their position within Google on that day.
 
-
+CREATE TABLE employment_history (
+    user_id INT,
+    employer VARCHAR(50),
+    position VARCHAR(50),
+    start_date DATE,
+    end_date DATE
+);
+INSERT INTO employment_history (user_id, employer, position, start_date, end_date)
+VALUES 
+    (1, 'Microsoft', 'developer', '2020-04-13', '2021-11-01'),
+    (1, 'Google', 'developer', '2021-11-01', NULL),
+    (2, 'Google', 'manager', '2021-01-01', '2021-01-11'),
+    (2, 'Microsoft', 'manager', '2021-01-11', NULL),
+    (3, 'Microsoft', 'analyst', '2019-03-15', '2020-07-24'),
+    (3, 'Amazon', 'analyst', '2020-08-01', '2020-11-01'),
+    (3, 'Google', 'senior analyst', '2020-11-01', '2021-03-04'),
+    (4, 'Google', 'junior developer', '2018-06-01', '2021-11-01'),
+    (4, 'Google', 'senior developer', '2021-11-01', NULL),
+    (5, 'Microsoft', 'manager', '2017-09-26', NULL),
+    (6, 'Google', 'CEO', '2015-10-02', NULL);
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
+-- Solution 1 - Simple and & between
+SELECT distinct user_id
+FROM employment_history
+WHERE employer = 'Google'
+AND '2021-11-01' BETWEEN start_date AND COALESCE(end_date, '9999-12-31')
+AND start_date != '2021-11-01'
+AND end_date != '2021-11-01';
 
+-- Solution 2 - 
+SELECT DISTINCT user_id 
+FROM employment_history
+WHERE employer = 'Google'
+AND start_date < '2021-11-01'
+AND (end_date IS NULL OR end_date > '2021-11-01');
 
 -- ==================================================================================================================================
 
