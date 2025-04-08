5c939a57e50d1f6e950816bcf29ba7c750bd4816
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2025-04-08 12:57:49 +0530

    Update DAY - 70.sql

--- MySQL Challenges/DAY - 61 to DAY - 90/DAY - 70.sql
+++ MySQL Challenges/DAY - 61 to DAY - 90/DAY - 70.sql
@@ -72,13 +72,82 @@ GROUP BY account_id;
 -- ==================================================================================================================================
 
 -- QUESTION : 2
--- 2. 
+-- 2. Same-Day Orders
+-- Identify users who started a session and placed an order on the same day. 
+-- For these users, the total number of orders placed on that day and the total order value for that day.
+-- Your output should include the user id, the session date, the total number of orders, and the total order value for that day.
 
+-- sessions table
+CREATE TABLE sessions (
+    session_id INT,
+    user_id INT,
+    session_date DATE
+);
 
+INSERT INTO sessions (session_id, user_id, session_date) VALUES
+(1, 1, '2024-01-01'),
+(2, 2, '2024-01-02'),
+(3, 3, '2024-01-05'),
+(4, 3, '2024-01-05'),
+(5, 4, '2024-01-03'),
+(6, 4, '2024-01-03'),
+(7, 5, '2024-01-04'),
+(8, 5, '2024-01-04'),
+(9, 3, '2024-01-05'),
+(10, 5, '2024-01-04');
+
+-- order_summary table
+CREATE TABLE order_summary (
+    order_id INT,
+    user_id INT,
+    order_value INT,
+    order_date DATE
+);
+
+INSERT INTO order_summary (order_id, user_id, order_value, order_date) VALUES
+(1, 1, 152, '2024-01-01'),
+(2, 2, 485, '2024-01-02'),
+(3, 3, 398, '2024-01-05'),
+(4, 3, 320, '2024-01-05'),
+(5, 4, 156, '2024-01-03'),
+(6, 4, 121, '2024-01-03'),
+(7, 5, 238, '2024-01-04'),
+(8, 5, 70, '2024-01-04'),
+(9, 3, 152, '2024-01-05'),
+(10, 5, 171, '2024-01-04');
 
 -- SOLUTION :------------------------------------------------------------------------------------------------------------------------
 
+-- SOLUTION 1 - Using INNER JOIN with GROUP BY
+SELECT 
+    os.user_id,
+    os.order_date AS session_date,
+    COUNT(DISTINCT os.order_id) AS total_no_orders,
+    SUM(os.order_value) AS total_order_value
+FROM sessions_tb st
+INNER JOIN order_summary os
+    ON st.user_id = os.user_id
+    AND st.session_date = os.order_date
+GROUP BY os.user_id, os.order_date
+ORDER BY os.user_id, os.order_date;
 
+-- SOLUTION 2 - Using CTE with DISTINCT Dates → Then Aggregation
+WITH same_day_users AS (
+    SELECT DISTINCT s.user_id, s.session_date
+    FROM sessions_tb s
+    JOIN order_summary o
+        ON s.user_id = o.user_id AND s.session_date = o.order_date
+)
+SELECT 
+    s.user_id,
+    s.session_date,
+    COUNT(o.order_id) AS total_orders,
+    SUM(o.order_value) AS total_order_value
+FROM same_day_users s
+JOIN order_summary o 
+    ON s.user_id = o.user_id AND s.session_date = o.order_date
+GROUP BY s.user_id, s.session_date
+ORDER BY s.user_id, s.session_date;
 
 -- ==================================================================================================================================
 
